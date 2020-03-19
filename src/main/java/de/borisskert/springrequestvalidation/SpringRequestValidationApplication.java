2e1a4a3e7f602c9e9f72997ae7bbac94399d7b44
Boris Skert <boris.skert@gmail.com>
2020-03-19 08:35:14 +0100

    Create project with Spring Initializr

---
+++ src/main/java/de/borisskert/springrequestvalidation/SpringRequestValidationApplication.java
@@ -0,0 +1,13 @@
+package de.borisskert.springrequestvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRequestValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRequestValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/borisskert/springrequestvalidation/SpringRequestValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package de.borisskert.springrequestvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRequestValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
